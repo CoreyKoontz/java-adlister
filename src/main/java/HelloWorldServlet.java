b7616556940d7e0863f66fa01dbb5adad72dc83c
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-06 22:23:05 -0500

    Adding HelloWorldServlet Exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
 
+    private int hitCount;
+
+    //Reset hitCount
+    public void init() {
+        hitCount = 0;
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        String name = req.getParameter("name" );
+        hitCount++;
+        PrintWriter out = res.getWriter();
+        if (name != null) {
+            out.println("<h1>Hello, " + name + "!</h1><h2>View Count: " + hitCount + "</h2>");
+        } else {
+            out.println("<h1>Hello, World!</h1></h1><h2>View Count: " + hitCount + "</h2>");
+        }
+    }
+
 }
