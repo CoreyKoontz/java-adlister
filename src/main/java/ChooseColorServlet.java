919db388e3b2aaab5df3587bf45774808dc5719b
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-08 13:24:16 -0500

    Adding servlet-challenge

--- src/main/java/ChooseColorServlet.java
+++ src/main/java/ChooseColorServlet.java
@@ -4,14 +4,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ChooseColorServlet", urlPatterns = "/choose-color")
 public class ChooseColorServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String color;
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        String output = "";
+        output += "<h1>Pick a Color:</h1>";
+        output += "<form method='POST' action='/choose-color'>";
+        output += "<label for='color'>Color: </label>";
+        output += "<input id='color' name=color type='text'>";
+        output += "<input type='submit'>";
+        output += "</form>";
+        out.println(output);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        color = req.getParameter("color");
+        resp.sendRedirect("/view-color?color=" + color);
 
+    }
 }
