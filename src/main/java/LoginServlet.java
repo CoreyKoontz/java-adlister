1ba419a8f4603b3a23ed233e596628a61248bda8
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-12 18:27:09 -0500

    Adding mvc-exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -9,11 +9,20 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/login.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getMethod().equalsIgnoreCase("post")) {
             String username = req.getParameter("username");
             String password = req.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
                 resp.sendRedirect("/profile");
+            } else {
+                resp.sendRedirect("/login");
+
             }
         }
     }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <jsp:include page="partials/head.jsp">
@@ -9,7 +10,7 @@
     <jsp:include page="partials/navbar.jsp" />
     <div class="container">
         <h1>Please Log In</h1>
-        <form action="/login.jsp" method="POST">
+        <form action="/login" method="POST">
             <div class="form-group">
                 <label for="username">Username</label>
                 <input id="username" name="username" class="form-control" type="text">
