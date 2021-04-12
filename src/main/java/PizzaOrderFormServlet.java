ba879c4f173986396b850d9a15985ce25d3bebe0
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-12 11:02:34 -0500

    Completed pizza exercise

--- src/main/java/PizzaOrderFormServlet.java
+++ src/main/java/PizzaOrderFormServlet.java
@@ -7,14 +7,14 @@ import java.io.IOException;
 
 @WebServlet(name = "PizzaOrderFormServlet", urlPatterns = "/pizza-order")
 public class PizzaOrderFormServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("order.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(req.getParameter("crust-selection"));
-        System.out.println(req.getParameter("sauce-selection"));
-        System.out.println(req.getParameter("size-selection"));
+        System.out.println("Crust: " + req.getParameter("crust-selection"));
+        System.out.println("Sauce: " + req.getParameter("sauce-selection"));
+        System.out.println("Size: " + req.getParameter("size-selection"));
     }
 }
