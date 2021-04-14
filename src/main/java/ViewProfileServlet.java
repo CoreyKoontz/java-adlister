d5b5434a1ffc28284f36b2b507fdcee39252e5ef
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-14 18:37:03 -0500

    Adding functionality to sessions and cookies exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
@@ -12,6 +13,9 @@ public class ViewProfileServlet extends HttpServlet {
             response.sendRedirect("/login");
             return;
         }
+        HttpSession session = request.getSession();
+        Object username = session.getAttribute("user");
+        request.setAttribute("name", username);
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
 }
