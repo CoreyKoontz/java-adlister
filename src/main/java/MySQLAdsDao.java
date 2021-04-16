3278bc8b5bdac11d9861a8e88f5a41a191998955
Corey Koontz <corey.michael.koontz@gmail.com>
2021-04-15 22:34:01 -0500

    Adding comments

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,2 +1,26 @@
-public class MySQLAdsDao implements Ads{
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLAdsDao implements Ads {
+
+// TODO---------------------------------------------------------- Initializing DB Connection:
+    private Connection connection = null;
+
+    public MySQLAdsDao(Config config) {
+
+        try {
+            DriverManager.registerDriver(new Driver());//<------------ Instantiating DB Driver
+            Connection connection = DriverManager.getConnection(//<--- Must only create connection once
+                    Config.getUrl(),//-------------\
+                    Config.getUsername(),//----------> Pulling in config values from Config.java
+                    Config.getPassword()//---------/
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
